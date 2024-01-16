f7ebcad430eb09d94082e596836259e29e9f181e
pardhu4039 <67086292+pardhu4039@users.noreply.github.com>
2024-01-16 15:33:07 +0530

    ec2.tf

--- modules/ec2/ec2.tf
+++ modules/ec2/ec2.tf
@@ -1,7 +1,9 @@
 resource "aws_instance" "example" {
   ami             = var.ami
   instance_type   = var.instance_type[0]
+  subnet_id       = "${aws_subnet.public_subnet.id}"
+  associate_public_ip_address = true
   tags = {
-    Name = "example-instance"
+    Name = "TerrafromDemo"
   }
 }
