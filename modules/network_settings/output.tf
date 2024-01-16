e066bceca1192965ed834983c9cec9073c870c22
pardhu4039 <67086292+pardhu4039@users.noreply.github.com>
2024-01-16 15:58:27 +0530

    output.tf

--- modules/network_settings/output.tf
+++ modules/network_settings/output.tf
@@ -1,3 +1,6 @@
-output "subnet_id" {
-  value = aws_subnet.example.id
+output "public_subnet_id" {
+  value = aws_subnet.public_subnet.id
+}
+output "private_subnet_id" {
+  value = aws_subnet.private_subnet.id
 }
