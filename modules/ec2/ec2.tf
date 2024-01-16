a6b262f9c65cd30bbe81973b8bb419d956a71a3a
pardhu4039 <67086292+pardhu4039@users.noreply.github.com>
2024-01-16 15:50:00 +0530

    ec2.tf

--- modules/ec2/ec2.tf
+++ modules/ec2/ec2.tf
@@ -1,4 +1,4 @@
-resource "aws_instance" "example" {
+resource "aws_instance" "my_instance" {
   ami             = var.ami
   instance_type   = var.instance_type[0]
   subnet_id       = "${aws_subnet.public_subnet.id}"
