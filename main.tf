db52b8858dc6b65088c0dad4d964fbb5609f5313
pardhu4039 <67086292+pardhu4039@users.noreply.github.com>
2024-01-18 17:27:06 +0530

    main.tf

--- main.tf
+++ main.tf
@@ -5,10 +5,17 @@ provider "aws" {
 }
 
 module "network" {
-  source = "./modules/network"
+  source             = "./modules/network"
+  vpc_cidr           = "10.0.0.0/16"
+  public_subnet_cidr  = "10.0.1.0/24"
+  private_subnet_cidr = "10.0.101.0/24"
 }
 
-module "ec2" {
-  source = "./modules/ec2"
-
+module "ec2_instance" {
+  source              = "./modules/ec2"
+  ami                 = "ami-0123456789abcdef0"
+  instance_type       = "t2.micro"
+  subnet_id           = "subnet-0123456789abcdef1"
+  security_group_ids  = ["sg-0123456789abcdef2"]
+  key_name            = "your-key-pair-name"
 }
