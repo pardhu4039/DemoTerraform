5671ad1f9a8ab2297f677ac9ebb6afd1080163d7
pardhu4039 <67086292+pardhu4039@users.noreply.github.com>
2024-01-18 17:28:02 +0530

    ec2.tf

--- modules/ec2/ec2.tf
+++ modules/ec2/ec2.tf
@@ -1,9 +1,11 @@
-resource "aws_instance" "my_instance" {
-  ami             = var.ami
-  instance_type   = var.instance_type[0]
-  subnet_id       = "${aws_subnet.public_subnet.id}"
+resource "aws_instance" "example" {
+  ami           = var.ami
+  instance_type = var.instance_type
+  subnet_id     = var.subnet_id
+  key_name      = var.key_name
   associate_public_ip_address = true
+
   tags = {
-    Name = "TerrafromDemo"
+    Name = "Terraform-instance"
   }
 }
