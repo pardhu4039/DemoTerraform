fae78593e53a4e00b285b8ed8d7d21b995650776
pardhu4039 <67086292+pardhu4039@users.noreply.github.com>
2024-01-19 09:27:37 +0530

    output.tf

--- modules/ec2/output.tf
+++ modules/ec2/output.tf
@@ -1,11 +1,7 @@
-output "instance_public_ip" {
-  value = aws_instance.example.public_ip
+output "public_ip" {
+  value = aws_instance.ec2_public.public_ip
 }
 
-output "instance_private_ip" {
-  value = aws_instance.example.private_ip
-}
-
-output "subnet_id" {
-  value = aws_subnet.public.id
+output "private_ip" {
+  value = aws_instance.ec2_private.private_ip
 }
