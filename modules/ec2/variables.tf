22412710855ba29469c5319df39944255b358ada
pardhu4039 <67086292+pardhu4039@users.noreply.github.com>
2024-01-19 09:28:12 +0530

    variables.tf

--- modules/ec2/variables.tf
+++ modules/ec2/variables.tf
@@ -1,20 +1,19 @@
-variable "ami" {
-  description = "The ID of the AMI to use for the EC2 instance"
+variable "namespace" {
+  type = string
 }
 
-variable "instance_type" {
-  description = "The type of EC2 instance"
+variable "vpc" {
+  type = any
 }
 
-variable "subnet_id" {
-  description = "The ID of the subnet where the EC2 instance will be launched"
+variable key_name {
+  type = string
 }
 
-variable "key_name" {
-  description = "The name of the key pair to use for the EC2 instance"
+variable "sg_pub_id" {
+  type = any
 }
 
-variable "security_group_ids" {
-  type        = list(string)
-  description = "List of security group IDs to attach to the EC2 instance"
+variable "sg_priv_id" {
+  type = any
 }
