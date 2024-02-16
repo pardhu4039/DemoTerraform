87e3a499dea08145f65fd960d359580387cf12e5
pardhu4039 <67086292+pardhu4039@users.noreply.github.com>
2024-02-16 12:47:54 +0530

    Update variables.tf

--- modules/networking/variables.tf
+++ modules/networking/variables.tf
@@ -1,3 +1,11 @@
 variable "namespace" {
   type = string
 }
+
+variable "private_subnet"{
+description = "Private subnet cidrs"
+}
+
+variable "public_subnet"{
+description = "Public subnet cidrs"
+}
