0ae0004e45fdbcf20f327e336a121ddbdd4abb0d
pardhu4039 <67086292+pardhu4039@users.noreply.github.com>
2024-02-23 17:44:51 +0530

    Updated network variables.tf

--- modules/networking/variables.tf
+++ modules/networking/variables.tf
@@ -1,19 +1,31 @@
 variable "namespace" {
   type = string
 }
-
-variable "private_subnets"{
-description = "Private subnet cidrs"
-type = list(string)
+variable "public_subnets" {
+  type = list(string)
+  default = ["10.0.101.0/24", "10.0.102.0/24"]
+  
+}
+variable "private_subnets" {
+  type = list(string)
+  default = ["10.0.1.0/24", "10.0.2.0/24"]
+  
+}
+variable "cidr" {
+  type = string
+  default = "10.0.0.0/16"
 }
 
-variable "public_subnets"{
-description = "Public subnet cidrs"
-type = list(string)
+variable "ssh_portnumber" {
+  type = number
+  default = 22
 }
-
-variable "main_cidr"{
-description = "Main cidr"
-type = string
+variable "all_port" {
+  type = number
+  default = 0
 }
-
+variable "allow_all" {
+  type = string
+  default = "0.0.0.0/0"
+  
+}
