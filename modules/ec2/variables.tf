812c89f9296eb757d2b4da601903415403d77cf7
pardhu4039 <67086292+pardhu4039@users.noreply.github.com>
2024-02-23 17:43:21 +0530

    Updated variables.tf

--- modules/ec2/variables.tf
+++ modules/ec2/variables.tf
@@ -17,3 +17,16 @@ variable "sg_pub_id" {
 variable "sg_priv_id" {
   type = any
 }
+
+variable "instance_type" {
+  type = string
+  default = "t2.micro"
+}
+
+variable "public_subnet_id" {
+  type =string
+}
+
+variable "private_subnet_id" {
+  type= string
+}
