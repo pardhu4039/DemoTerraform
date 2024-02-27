eaa9ce91f80bb538eeabd84272dbc1a04e358ee5
pardhu4039 <67086292+pardhu4039@users.noreply.github.com>
2024-02-27 16:24:12 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,13 @@
 module "networking" {
-  source    = "./modules/networking"
-  namespace = var.namespace
+  source          = "./modules/networking"
+  namespace       = var.namespace
+  cidr            = var.cidr
+  public_subnets  = var.public_subnets
+  private_subnets = var.private_subnets
+  allow_all       = var.allow_all
+  all_port        = var.all_port
+  ssh_portnumber  = var.ssh_portnumber
+  environment     = var.environment
 }
 
 module "ssh-key" {
@@ -9,13 +16,13 @@ module "ssh-key" {
 }
 
 module "ec2" {
-  source     = "./modules/ec2"
-  namespace  = var.namespace
-  vpc        = module.networking.vpc
-  sg_pub_id  = module.networking.sg_pub_id
-  sg_priv_id = module.networking.sg_priv_id
-  key_name   = module.ssh-key.key_name
-  private_subnet_id = module.networking.private_subnet_ids[0]
-  public_subnet_id = module.networking.public_subnet_ids[0]
+  source            = "./modules/ec2"
+  namespace         = var.namespace
+  instance_type     = var.instance_type
+  vpc               = data.aws_vpc.vpc.id
+  sg_pub_id         = data.aws_security_groups.public_sg.id
+  sg_priv_id        = data.aws_security_groups.private_sg.id
+  key_name          = module.ssh-key.key_name
+  private_subnet_id = data.aws_subnets.private.ids
+  public_subnet_id  = data.aws_subnets.public.ids
 }
-
