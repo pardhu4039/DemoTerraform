3ec2e5efc37f7d1ee3e57a78ef8411b2d5c68308
pardhu4039 <67086292+pardhu4039@users.noreply.github.com>
2024-02-27 16:25:30 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -9,11 +9,43 @@ variable "access_key" {
 }
 
 variable "secret_key" {
-  type = string  
+  type = string
 }
 
 variable "region" {
   description = "AWS region"
   default     = "us-east-1"
   type        = string
+}
+
+variable "public_subnets" {
+  type    = list(string)
+  default = []
+
+}
+variable "private_subnets" {
+  type    = list(string)
+  default = []
+}
+variable "cidr" {
+  type = string
+}
+
+variable "instance_type" {
+  type = string
+}
+
+variable "ssh_portnumber" {
+  type = number
+}
+
+variable "all_port" {
+  type = number
+}
+variable "allow_all" {
+  type = string
+}
+variable "environment" {
+  type = string
+
 }
